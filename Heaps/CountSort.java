34f1cb51eadddce845cccf95e525d57bfc5d48a0
Saurav Raj <113482311+rajsaurav4055@users.noreply.github.com>
2023-09-26 10:30:50 +0530

    Day 2

--- Heaps/CountSort.java
+++ Heaps/CountSort.java
@@ -1,7 +1,7 @@
 
 //See its explanation from code with harry YT channel.. Very very easy concept
 //Constraint : Valid for only positive integers BUT we have a modified count sort to include the negative integers as well in LC 973- Kth largest element in an array
-
+// Also doesn't work for decimal values
 //TC: O(N)
 //SC: O(maxValue) where maxValue is the maximum value in the array
 public class CountSort {
