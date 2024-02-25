cb71a3390ecf9cf4c79aae2a316afcfb70ee344d
Saurav Raj <113482311+rajsaurav4055@users.noreply.github.com>
2024-02-24 23:26:51 -0500

    Bit Manipulation- Reverse Integer

---
+++ Bit Manipulation/LC 190 | Reverse Bits.java
@@ -0,0 +1,17 @@
+package Bit Manipulation;
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res=0;
+        for(int i=0; i<32; i++){
+            int bit=(n >> i) & 1;
+            res= res | (bit << (31 - i));
+        }
+        return res;
+    }
+}
+
+//TC: O(1)
+//SC: O(1)
+//Nice explanation by leetcode
