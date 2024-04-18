b4a3ad8537c2f181d2cd76d71eb21786a2a50bee
Saurav Raj <113482311+rajsaurav4055@users.noreply.github.com>
2024-04-18 17:29:43 -0400

    Maximum Product Subarray

---
+++ DP/1-D DP/LC | Maximum Product Subarray.java
@@ -0,0 +1,21 @@
+//one of a kind solutions
+//prefix and suffix sums lo and then dono me se se max jo hai wo.
+//if prefix or suffix product becomes 0 toh update it to 1 because 0 k baad we reset the product
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int prefix=1;
+        int suffix=1;
+        int max=Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            if(prefix==0)prefix=1;
+            if(suffix==0)suffix=1;
+            
+            prefix*=nums[i];
+            suffix*=nums[n-i-1];
+            max=Math.max(max, Math.max(prefix,suffix));
+        }
+        return max;
+    }
+}
